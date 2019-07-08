cb815e987e74d2fb0896b41577254c7e217cb1da
Geno Shamugia <48299520+shamo0@users.noreply.github.com>
2019-07-08 10:59:09 -0700

    Add files via upload

---
+++ treapTest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from treaps import *
+
+def main():
+    tree = TreapSet()
+    tree.add(3)
+    # tree.add(3)
+    tree.add(6)
+    tree.add(77)
+    tree.add(1)
+    print(3 in tree)
+    print(5 in tree)
+    # print(7 in tree)
+    # print(tree.height())
+    # print(len(tree))
+
+
+
+
+main()
